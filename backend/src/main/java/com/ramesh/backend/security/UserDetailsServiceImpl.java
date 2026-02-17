1dabe8c286d24886e85510f4b149b2cd5ff330c5
rameshpokahrel21 <pokharelramesh@yahoo.com>
2026-02-17 08:37:50 -0500

    UserDetailsService implementation class added

--- backend/src/main/java/com/ramesh/backend/security/UserDetailsServiceImpl.java
+++ backend/src/main/java/com/ramesh/backend/security/UserDetailsServiceImpl.java
@@ -1,5 +1,7 @@
 package com.ramesh.backend.security;
 
+import com.ramesh.backend.entity.User;
+import com.ramesh.backend.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,8 +9,17 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+        return UserDetailsImpl.build(user);
     }
 }

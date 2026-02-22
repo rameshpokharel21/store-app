c63c1175902a5a4833e65a9bd20d595d99f92854
rameshpokahrel21 <pokharelramesh@yahoo.com>
2026-02-22 12:50:08 -0500

    AuthController class implemented

--- backend/src/main/java/com/ramesh/backend/controller/AuthController.java
+++ backend/src/main/java/com/ramesh/backend/controller/AuthController.java
@@ -1,16 +1,20 @@
 package com.ramesh.backend.controller;
 
 import com.ramesh.backend.dto.request.LoginRequest;
+import com.ramesh.backend.dto.request.RegisterRequest;
 import com.ramesh.backend.dto.response.LoginResponse;
+import com.ramesh.backend.dto.response.MessageResponse;
+import com.ramesh.backend.dto.response.UserResponse;
 import com.ramesh.backend.security.service.AuthService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins="${cors-allowed-origins}", allowCredentials = "true")
 public class AuthController {
 
     private final AuthService authService;
@@ -24,5 +28,25 @@ public class AuthController {
                                    HttpServletResponse response){
         LoginResponse loginResponse = authService.login(loginRequest, response);
         return ResponseEntity.ok(loginResponse);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@Valid @RequestBody RegisterRequest request){
+        UserResponse response = authService.register(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletResponse response){
+        authService.logout(response);
+        return ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .body(new MessageResponse("Logged out successfully."));
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<?> refreshToken(HttpServletRequest request, HttpServletResponse response){
+        authService.refreshAccessToken(request, response);
+        return ResponseEntity.ok(new MessageResponse("Token refreshed successfully."));
     }
 }

6926d0e47e45308c4560373963d5c86b3333ef26
rameshpokahrel21 <pokharelramesh@yahoo.com>
2026-02-22 21:31:08 -0500

    Password validator updated

--- backend/src/main/java/com/ramesh/backend/validation/PasswordMatchValidator.java
+++ backend/src/main/java/com/ramesh/backend/validation/PasswordMatchValidator.java
@@ -4,8 +4,6 @@ import com.ramesh.backend.dto.request.RegisterRequest;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-import java.lang.annotation.Annotation;
-
 public class PasswordMatchValidator implements ConstraintValidator<PasswordMatch, Object> {
     @Override
     public void initialize(PasswordMatch constraintAnnotation) {
@@ -16,8 +14,15 @@ public class PasswordMatchValidator implements ConstraintValidator<PasswordMatch
     public boolean isValid(Object obj, ConstraintValidatorContext constraintValidatorContext) {
         if(obj instanceof RegisterRequest){
             RegisterRequest request = (RegisterRequest) obj;
-            return request.password() != null &&
+            boolean valid =  request.password() != null &&
                     request.password().equals(request.confirmPassword());
+            if(!valid){
+                constraintValidatorContext.disableDefaultConstraintViolation();
+                constraintValidatorContext.buildConstraintViolationWithTemplate(constraintValidatorContext.getDefaultConstraintMessageTemplate())
+                        .addPropertyNode("confirmPassword")
+                        .addConstraintViolation();
+            }
+            return valid;
         }
         return true;
     }

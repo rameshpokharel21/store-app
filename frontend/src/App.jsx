3746a4d8978ddd05bdcf8f682b6621aff27da318
rameshpokahrel21 <pokharelramesh@yahoo.com>
2026-02-23 10:33:35 -0500

    frontend project started with vite and react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='text-center font-semibold text-blue-500'>
+      Hello Store App with React and Vite
+    </div>
+  )
+}
+
+export default App

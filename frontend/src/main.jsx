b348902f3dee4732f9c0bda55efec24ae54471ff
rameshpokahrel21 <pokharelramesh@yahoo.com>
2026-02-23 14:08:35 -0500

    added QueryClientProvider, AuthProvider, ErrorProvider in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,34 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import {ErrorProvider} from "./utils/ErrorProvider.jsx";
+import AuthProvider from './contexts/AuthProvider.jsx'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 2,
+      retryDelay: attemptIndex => Math.min(1000*2**attemptIndex, 30000),
+      staleTime: 2 * 60 * 1000,
+      gcTime: 10*60*1000,
+    },
+    mutations: {
+      retry: 1,
+    }
+  }
+});
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <QueryClientProvider client ={queryClient}>
+    <ErrorProvider>
+      <AuthProvider>
+         <StrictMode>
+          <App />
+        </StrictMode>
+      </AuthProvider>
+    </ErrorProvider>
+  </QueryClientProvider>
 )
